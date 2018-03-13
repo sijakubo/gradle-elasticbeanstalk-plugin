3ddc37e83b61c1eca9eca9f22cac432a25045be5
Jan Sauer <jan@jansauer.de>
2018-03-13 16:23:12 +0100

    Reformat the code

--- src/main/groovy/de/jansauer/elasticbeanstalk/ElasticBeanstalkExtension.groovy
+++ src/main/groovy/de/jansauer/elasticbeanstalk/ElasticBeanstalkExtension.groovy
@@ -5,11 +5,11 @@ import org.gradle.api.provider.Property
 
 class ElasticBeanstalkExtension {
 
-    final Property<String> applicationName
-    final Property<Integer> versionToPreserve
+  final Property<String> applicationName
+  final Property<Integer> versionToPreserve
 
-    ElasticBeanstalkExtension(Project project) {
-        applicationName = project.objects.property(String)
-        versionToPreserve = project.objects.property(Integer)
-    }
+  ElasticBeanstalkExtension(Project project) {
+    applicationName = project.objects.property(String)
+    versionToPreserve = project.objects.property(Integer)
+  }
 }
